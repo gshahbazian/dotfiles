ad91a43cc007deda3eb7d7e8f8adc05a22caf2ce
gshahbazian <gabeshahbazian@gmail.com>
2025-06-22 13:10:51 -0700

    add silent = true

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -6,16 +6,16 @@ vim.keymap.set("i", "jk", "<ESC>", { silent = true })
 
 vim.keymap.set("n", "<leader>sf", function()
   Snacks.picker.grep({ root = false, args = { "-F" } })
-end, { desc = "String match" })
+end, { silent = true, desc = "String match" })
 
 vim.keymap.set("n", "<leader><BS>", function()
   Snacks.bufdelete()
-end, { desc = "Close Buffer" })
+end, { silent = true, desc = "Close Buffer" })
 
-vim.keymap.set({ "n", "v", "x" }, "<leader>y", '"+y', { desc = "Copy to system clipboard" })
+vim.keymap.set({ "n", "v", "x" }, "<leader>y", '"+y', { silent = true, desc = "Copy to system clipboard" })
 
 -- cmd+c not working in ghostty : (
 -- vim.keymap.set("v", "<D-c>", '"+y', { desc = "Copy to system clipboard (Cmd+C)" })
 
-vim.keymap.set("n", "<D-s>", "<cmd>w<cr>", { desc = "Save" })
-vim.keymap.set("i", "<D-s>", "<Esc><cmd>w<cr>", { desc = "Save" })
+vim.keymap.set("n", "<D-s>", "<cmd>w<cr>", { silent = true, desc = "Save" })
+vim.keymap.set("i", "<D-s>", "<Esc><cmd>w<cr>", { silent = true, desc = "Save" })
