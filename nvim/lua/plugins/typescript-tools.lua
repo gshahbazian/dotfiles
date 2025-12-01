e73df7922694fee4e3b71f2f1948188085b02614
gshahbazian <gabeshahbazian@gmail.com>
2025-12-01 15:22:59 -0800

    add keybinds for tstools

--- nvim/lua/plugins/typescript-tools.lua
+++ nvim/lua/plugins/typescript-tools.lua
@@ -2,19 +2,36 @@ return {
   {
     "pmizio/typescript-tools.nvim",
     dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-    opts = {
-      settings = {
-        tsserver_file_preferences = {
-          importModuleSpecifierPreference = "non-relative",
-          -- includeInlayParameterNameHints = "literals",
-          -- includeInlayFunctionParameterTypeHints = true,
-          -- includeInlayFunctionLikeReturnTypeHints = true,
-          -- includeInlayVariableTypeHints = false,
-          -- includeInlayPropertyDeclarationTypeHints = true,
-          -- includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+    config = function()
+      require("typescript-tools").setup({
+        on_attach = function(_, bufnr)
+          local map = function(lhs, rhs, desc)
+            vim.keymap.set("n", lhs, rhs, {
+              buffer = bufnr,
+              silent = true,
+              noremap = true,
+              desc = desc,
+            })
+          end
+
+          map("<leader>cM", "<cmd>TSToolsAddMissingImports<CR>", "TS: Add Missing Imports")
+          map("<leader>cD", "<cmd>TSToolsFixAll<CR>", "TS: Fix all diagnostics")
+        end,
+
+        settings = {
+          separate_diagnostic_server = true,
+          publish_diagnostic_on = "insert_leave",
+          tsserver_file_preferences = {
+            importModuleSpecifierPreference = "non-relative",
+            includeCompletionsForModuleExports = true,
+          },
+          tsserver_format_options = {
+            allowIncompleteCompletions = false,
+            allowRenameOfImportPath = false,
+          },
         },
-      },
-    },
+      })
+    end,
   },
 
   {
