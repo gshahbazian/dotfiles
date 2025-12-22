95e9cbb058bd66d64014aad176fcf5a4fb1d3483
gshahbazian <gabeshahbazian@gmail.com>
2025-12-22 13:55:45 -0800

    window spacing tweak

--- nvim/lua/plugins/typescript-tools.lua
+++ nvim/lua/plugins/typescript-tools.lua
@@ -34,22 +34,22 @@ return {
       })
     end,
   },
-
-  {
-    "nvim-mini/mini.icons",
-    enabled = false,
-    opts = {
-      file = {
-        [".eslintrc.js"] = { glyph = "󰱺", hl = "MiniIconsYellow" },
-        [".node-version"] = { glyph = "", hl = "MiniIconsGreen" },
-        [".prettierrc"] = { glyph = "", hl = "MiniIconsPurple" },
-        [".yarnrc.yml"] = { glyph = "", hl = "MiniIconsBlue" },
-        ["eslint.config.js"] = { glyph = "󰱺", hl = "MiniIconsYellow" },
-        ["package.json"] = { glyph = "", hl = "MiniIconsGreen" },
-        ["tsconfig.json"] = { glyph = "", hl = "MiniIconsAzure" },
-        ["tsconfig.build.json"] = { glyph = "", hl = "MiniIconsAzure" },
-        ["yarn.lock"] = { glyph = "", hl = "MiniIconsBlue" },
-      },
-    },
-  },
+  --
+  -- {
+  --   "nvim-mini/mini.icons",
+  --   enabled = false,
+  --   opts = {
+  --     file = {
+  --       [".eslintrc.js"] = { glyph = "󰱺", hl = "MiniIconsYellow" },
+  --       [".node-version"] = { glyph = "", hl = "MiniIconsGreen" },
+  --       [".prettierrc"] = { glyph = "", hl = "MiniIconsPurple" },
+  --       [".yarnrc.yml"] = { glyph = "", hl = "MiniIconsBlue" },
+  --       ["eslint.config.js"] = { glyph = "󰱺", hl = "MiniIconsYellow" },
+  --       ["package.json"] = { glyph = "", hl = "MiniIconsGreen" },
+  --       ["tsconfig.json"] = { glyph = "", hl = "MiniIconsAzure" },
+  --       ["tsconfig.build.json"] = { glyph = "", hl = "MiniIconsAzure" },
+  --       ["yarn.lock"] = { glyph = "", hl = "MiniIconsBlue" },
+  --     },
+  --   },
+  -- },
 }
