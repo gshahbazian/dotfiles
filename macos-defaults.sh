fd2209c16e1551f98c8eab82beb804f1a8460e64
gshahbazian <gabeshahbazian@gmail.com>
2026-03-07 10:52:09 -0800

    make mac defaults executable

--- macos-defaults.sh
+++ macos-defaults.sh
@@ -1,21 +1,26 @@
-# dock speed (killall Dock; to apply)
-defaults write com.apple.dock autohide-delay -float 0
+#!/usr/bin/env bash
+
+# Dock "removal"
+defaults write com.apple.dock autohide-delay -float 1.0
 defaults write com.apple.dock autohide-time-modifier -float 0.5
 
-# better smoothing
+# Better smoothing
 defaults -currentHost write -g AppleFontSmoothing -int 0
 
-# key repeat
+# Key repeat
 # The system has to be restarted for changes to take effect.
 # All values copied from https://mac-key-repeat.zaymon.dev
-
 defaults write -g InitialKeyRepeat -int 15
 defaults write -g KeyRepeat -int 2
 defaults write -g ApplePressAndHoldEnabled -bool false
 
-# not sure about these
+# Misc
 chflags nohidden ~/Library
 defaults write -g NSDisableAutomaticTermination -bool true
 defaults write -g AppleShowAllExtensions -bool true
 defaults write com.apple.finder AppleShowAllFiles -bool true
 defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+# Restart affected apps
+killall Dock
+killall Finder
